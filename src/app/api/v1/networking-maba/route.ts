4962b5fb4ae66bb71ad35fb0f166db38a47e52ab
Danniel-Ang <danniel@ui.ac.id>
2025-07-26 00:57:14 +0700

    refactor: fixing all status code missing

--- src/app/api/v1/networking-maba/route.ts
+++ src/app/api/v1/networking-maba/route.ts
@@ -2,6 +2,43 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 import serverResponse, { InvalidHeadersResponse } from "@/utils/serverResponse";
 
+export async function GET(req: NextRequest) {
+  const userId = req.headers.get("X-User-Id");
+  if (!userId) {
+    return InvalidHeadersResponse;
+  }
+  await prisma.$connect();
+  const conns = await prisma.networkingTask.findMany({
+    where: {
+      fromId: +userId,
+    },
+    include: {
+      to: {
+        omit: {
+          password: false,
+        },
+      },
+      from: {
+        omit: {
+          password: false,
+        },
+      },
+      questions: {
+        include: {
+          question: true,
+        },
+      },
+    },
+  });
+  await prisma.$disconnect();
+  return serverResponse({
+    success: true,
+    message: "Berhasil mengambil data networking maba",
+    data: conns,
+    status: 200,
+  });
+}
+
 /**
  * @swagger
  * /api/v1/networking-maba:
@@ -107,40 +144,3 @@ import serverResponse, { InvalidHeadersResponse } from "@/utils/serverResponse";
  *                   type: integer
  *                   example: 400
  */
-
-export async function GET(req: NextRequest) {
-  const userId = req.headers.get("X-User-Id");
-  if (!userId) {
-    return InvalidHeadersResponse;
-  }
-  await prisma.$connect();
-  const conns = await prisma.networkingTask.findMany({
-    where: {
-      fromId: +userId,
-    },
-    include: {
-      to: {
-        omit: {
-          password: false,
-        },
-      },
-      from: {
-        omit: {
-          password: false,
-        },
-      },
-      questions: {
-        include: {
-          question: true,
-        },
-      },
-    },
-  });
-  await prisma.$disconnect();
-  return serverResponse({
-    success: true,
-    message: "Berhasil mengambil data networking maba",
-    data: conns,
-    status: 200,
-  });
-}
